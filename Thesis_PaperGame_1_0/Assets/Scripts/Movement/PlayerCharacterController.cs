18bf3645548d325a6ab13d388809cd4cbc20e704
ghostrecollection <heartforbrains126@gmail.com>
2026-09-23 01:00:27 -0400

    Polished Movement

--- Thesis_PaperGame_1_0/Assets/Scripts/Movement/PlayerCharacterController.cs
+++ Thesis_PaperGame_1_0/Assets/Scripts/Movement/PlayerCharacterController.cs
@@ -13,11 +13,23 @@ public class PlayerCharacterController : MonoBehaviour
     //Movements
     [Header("Movement Settings")]
     [Space(10)]
-    [SerializeField] float speed = 5f;
-    [SerializeField] float rotationSmoothSpeed = 20f;
+    [SerializeField] float currentSpeed;
+    [SerializeField] float idleSpeed = 0f;
+    [SerializeField] float walkSpeed = 5f;
+    [SerializeField] float runSpeed = 8f;
+    [Space(10)]
+    //[SerializeField] float jumpForce = 7f;
+    //bool jumpActive;
+
+    [Header("Movement Transitions")]
+    [Space(10)]
+    [SerializeField] float movementSmoothSpeed = 3f;
+    [SerializeField] float rotationSmoothSpeed = 10f;
+    
 
 
-    // Start is called once before the first frame update.
+
+    // --- START ---
     void Start()
     {
         // Get PlayerInputManager Script from GameObject.
@@ -26,22 +38,54 @@ public class PlayerCharacterController : MonoBehaviour
         controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame.
+    // --- UPDATE ---
     void Update()
     {
+        Debug.Log($"currentSpeed: {currentSpeed}");
+        OnMove();
+
+    }
+
+    // --- MOVEMENT ---
+    void OnMove()
+    {
+        currentSpeed = idleSpeed;
+        
         // Creates a Vector 3 using our stored values from "OnMove" Function-
         // -in our Input System and applies them to the x and z.
-        Vector3 targetDirection = new Vector3(pInput.move.x, 0, pInput.move.y);
-        // Creates a Quaternion variable that makes the targetRotation the targetDirection Input.
-        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
-        if(pInput.move != Vector2.zero)
-        {
-            // Changes the direction the character is facing and smooths the change.
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSmoothSpeed * Time.deltaTime);
-        }
-        
+        Vector3 inputDir = new Vector3(pInput.move.x, 0, pInput.move.y);
+        // Target rotation -- player rotates to direction input
+        float targetRotation = 0;
 
+        if (pInput.move != Vector2.zero)
+        {
+            // Running
+            if (pInput.run)
+            {
+                currentSpeed = runSpeed;
+                //float movementSmoothedSpeed = Mathf.Lerp(currentSpeed, runSpeed, Time.deltaTime * movementSmoothSpeed);
+                //currentSpeed = movementSmoothedSpeed;
+            }
+            else
+            {
+                currentSpeed = walkSpeed;
+                //float movementSmoothedSpeed = Mathf.Lerp(currentSpeed, walkSpeed, Time.deltaTime * movementSmoothSpeed);
+                //currentSpeed = movementSmoothedSpeed;
+            }
+
+            
+
+
+            // Creates a Quaternion variable that makes the targetRotation the targetDirection Input.
+            targetRotation = Quaternion.LookRotation(inputDir).eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0, targetRotation, 0);
+            // Changes the direction the character is facing and smooths the change.
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSmoothSpeed * Time.deltaTime);
+
+        }
+
+        Vector3 targetDirection = Quaternion.Euler(0, targetRotation, 0) * Vector3.forward;
         // Moves the controller based of the targetDirection values * player speed over time.
-        controller.Move(targetDirection * speed * Time.deltaTime);
+        controller.Move(inputDir * currentSpeed * Time.deltaTime);
     }
 }
